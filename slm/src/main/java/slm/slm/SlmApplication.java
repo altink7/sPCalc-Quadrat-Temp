6310bc9ac4124ec18462277460d6406da7f19eb7
altin <kelmendialtin48@gmail.com>
2022-06-19 15:18:19 +0200

    Spring added

---
+++ slm/src/main/java/slm/slm/SlmApplication.java
@@ -0,0 +1,13 @@
+package slm.slm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlmApplication.class, args);
+    }
+
+}
---
+++ slm/src/test/java/slm/slm/SlmApplicationTests.java
@@ -0,0 +1,13 @@
+package slm.slm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
