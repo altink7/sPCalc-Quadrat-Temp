a84be98eb5117b3021dc5064f50097a786e2ffc1
altin <kelmendialtin48@gmail.com>
2022-08-03 20:15:46 +0200

    unittest added, something tested :)

--- slm/src/test/java/slm/slm/UnitTests/CapitalTest.java
+++ slm/src/test/java/slm/slm/UnitTests/CapitalTest.java
@@ -7,14 +7,22 @@ import slm.slm.CapitalController;
 public class CapitalTest {
 
     @Test
-    public void testCapital(){
+    public void testCapitalAustria(){
         CapitalController controller = new CapitalController();
 
         String actual= controller.capital("Austria");
         String excepted ="Vienna";
 
-
-
         Assert.assertEquals(actual,excepted,"should be Vienna!");
+    }
+
+    @Test
+    public void testCapitalGermany(){
+        CapitalController controller = new CapitalController();
+
+        String actual= controller.capital("Germany");
+        String excepted ="Berlin";
+
+        Assert.assertEquals(actual,excepted,"should be Berlin!");
     }
 }
